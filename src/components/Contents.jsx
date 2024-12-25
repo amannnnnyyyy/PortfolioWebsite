172e58ca22818a2c64668283ae1b0222f41ba887
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-25 11:16:03 +0300

    toggle menu

--- src/components/Contents.jsx
+++ src/components/Contents.jsx
@@ -1,11 +1,11 @@
 import React from 'react'
 
 const Contents = ({visibility}) => {
-  const li_styles = "py-1 hover:bg-white-700 hover:text-black-100"
+  const li_styles = "py-1 hover:text-sky-700  cursor-pointer"
   const attributes = ["Home","About","Services","Contact","Blog","Portfolio","FAQ","Testimonials"]
 
   return (
-      <div className='flex justify-end z-30'>
+      <div className='flex justify-end z-30 mr-4'>
         {visibility && (
         <div className='bg-transparent text-white-800 border border-gray-300 p-2 z-30'>
             <ul className='list-none m-0 p-0'>
--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -5,11 +5,19 @@ const NavBar = () => {
     const [isVisible, setIsVisible] = useState(false)
     const name = "Amanuel"
     const [hamburgerIcon, setHamburgerIcon] = useState("/assets/menu.svg")
+    const [nightModePic, setNightModePic] = useState("/assets/night.png")
+
 
     const toggleHamburger = () => {
         setHamburgerIcon(hamburgerIcon === "/assets/menu.svg"? "/assets/close.svg" : "/assets/menu.svg")
-        setIsVisible(!isVisible)
+        setIsVisible((isVisible)=>!isVisible)
     }
+
+    const toggleNightMode = () => {
+        setNightModePic(nightModePic === "/assets/night.png"? "/assets/day.png" : "/assets/night.png")
+        document.body.classList.toggle('dark-mode')
+    }
+
   return (
     <header className='fixed top-0 left-0 right-0 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
@@ -19,10 +27,11 @@ const NavBar = () => {
                     {name}
                 </a>
                
-                <button onClick={toggleHamburger}>
-                    <img src={hamburgerIcon} alt="toggle" className='w-6 h-6'/>
+                <button className='flex gap-5 items-center'>
+                    <img className='opacity-90 hover:opacity-100' src={nightModePic} width={90} height={1} alt="day mode" onClick={toggleNightMode}/>
+                    <img src={hamburgerIcon} alt="toggle" className='w-6 h-6 opacity-90 hover:opacity-100' onClick={toggleHamburger}/>
                 </button>
-                
+            
                 
             </div>
             <Contents visibility = {isVisible}/>
