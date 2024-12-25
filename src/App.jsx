4072dce9a813c8f18d774f74a2924095b21dd2d0
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-25 13:50:00 +0300

    about to add 3d model

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,13 @@
 import React from 'react'
 import NavBar from './sections/NavBar'
 import Contents from './components/Contents'
+import Hero from './sections/Hero'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <NavBar/>
+      <Hero/>
     </main>
   )
 }
---
+++ src/sections/Hero.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Hero = () => {
+  return (
+    <div className='bg-white-700'>Hero</div>
+  )
+}
+
+export default Hero
