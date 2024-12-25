c277a35bea39409ff55f7fff4d5a88d815eccdd8
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-25 08:25:56 +0300

    contents

---
+++ src/components/Contents.jsx
@@ -0,0 +1,21 @@
+import React from 'react'
+
+const Contents = () => {
+  return (
+    <div>
+        <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Services</li>
+            <li>Contact</li>
+            <li>Blog</li>
+            <li>Portfolio</li>
+            <li>FAQ</li>
+            <li>Testimonials</li>
+            
+        </ul>
+    </div>
+  )
+}
+
+export default Contents
--- src/sections/NavBar.jsx
+++ src/sections/NavBar.jsx
@@ -1,17 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
 
 const NavBar = () => {
+    const name = "Amanuel"
+    const [hamburgerIcon, setHamburgerIcon] = useState("/assets/menu.svg")
+
+    const toggleHamburger = () => {
+        setHamburgerIcon(hamburgerIcon === "/assets/menu.svg"? "/assets/close.svg" : "/assets/menu.svg")
+    }
   return (
     <header className='fixed top-0 left-0 right-0 z-50 bg-black/90'>
         <div className='max-w-7xl mx-auto'>
             <div className='flex justify-between items-center py-5 mx-auto c-space'>
+
                 <a href="/" className='text-neutral-400 font-bold text-xl hover:text-white transition-colors'>
-                    Amanuel
+                    {name}
                 </a>
-                <button>
-                    <img src={} alt="" />
+               
+                <button onClick={toggleHamburger}>
+                    <img src={hamburgerIcon} alt="toggle" className='w-6 h-6'/>
                 </button>
+                
             </div>
+
         </div>
     </header>
   )
