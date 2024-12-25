06adb65deb2ddf3a93ce8bbde2db9a3aa6aa9971
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-25 16:39:23 +0300

    model added to hero

--- src/components/CanvasLoader.jsx
+++ src/components/CanvasLoader.jsx
@@ -1,8 +1,25 @@
+import { Html, useProgress } from '@react-three/drei'
 import React from 'react'
 
 const CanvasLoader = () => {
+    const {progress} = useProgress()
   return (
-    <div>CanvasLoader</div>
+    <Html
+        as='div' center style={
+            {
+                display: 'flex',
+                justifyContent: 'center',
+                alignItems: 'center',
+                flexDirection: 'column',
+                zIndex:50
+            }
+        }
+        >
+            <span className='canvas-loader'/>
+            <p style={{fontSize: 14, color: '#ffffff', fontWeight: 800, marginTop: 40, padding: '5px 10px'}}>
+                {progress!=0?`${progress.toFixed(2)+' %'}`:`Loading...`}
+                </p>
+        </Html>
   )
 }
 
--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -1,35 +1,35 @@
-import { OrbitControls, PerspectiveCamera } from '@react-three/drei'
-import { Canvas } from '@react-three/fiber'
-import React, { Suspense } from 'react'
-import HackerRoom from '../components/HackerRoom'
+import { OrbitControls, PerspectiveCamera } from '@react-three/drei';
+import { Canvas } from '@react-three/fiber';
+import React, { Suspense } from 'react';
+import HackerRoom from '../components/HackerRoom';
+import CanvasLoader from '../components/CanvasLoader';
 
 const Hero = () => {
   return (
     <section className='min-h-screen w-full flex flex-col relative'>
-        <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3'>
-            <p className='sm:text-3xl text-2xl text-white font-medium text-center font-generalsans'>
-                Hi, I am Amanuel 
-                    <span className='waving-hand'>👋</span>
-            </p>
-            <p className='hero_tag text-gray_gradient'>
-                Software Developer <span className='hidden sm:inline'>|</span> ML Engineer
-            </p>
-        </div>
+      <div className='w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3 z-10 relative'>
+        <p className='sm:text-3xl text-2xl text-white font-medium text-center font-generalsans'>
+          Hi, I am Amanuel 
+          <span className='waving-hand'>👋</span>
+        </p>
+        <p className='hero_tag text-gray_gradient bg-black bg-opacity-50 p-2 rounded-md text-center'>
+          Software Developer <span className='hidden sm:inline'>|</span> ML Engineer
+        </p>
+      </div>
 
-        <div className='w-full h-full position-absolute inset-0'>
-            <Canvas className='w-full h-full'>
-                <Suspense fallback={null}>
-
-                </Suspense>
-                <PerspectiveCamera makeDefault position={[0,0,30]}/>
-                <OrbitControls enableZoom enablePan enableRotate/>
-                <directionalLight position={[1,1,1]} intensity={1.5} color={'#9cdba6'}/>
-                <color attach="background" args={['#F0F0F0']} />
-                <HackerRoom/>
-            </Canvas>
-        </div>
+      <div className='w-full h-full absolute inset-0 z-0'>
+        <Canvas className='w-full h-full'>
+          <Suspense fallback={<CanvasLoader />}>
+            <PerspectiveCamera makeDefault position={[0, 0, 30]} />
+            <OrbitControls enableZoom enablePan enableRotate />
+            <ambientLight intensity={1} />
+            <directionalLight position={[10, 10, 10]} intensity={0.5} color={'#9cdba6'} />
+            <HackerRoom scale={0.2} position={[0, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
+          </Suspense>
+        </Canvas>
+      </div>
     </section>
-  )
+  );
 }
 
-export default Hero
+export default Hero;
