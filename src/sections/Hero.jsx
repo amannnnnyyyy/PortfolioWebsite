f98b1419213ee6404e3dbead8a9fbcffed62e7fd
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-26 09:56:42 +0300

    eslint correction

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -17,14 +17,14 @@ const Hero = () => {
         </p>
       </div>
 
-      <div className='w-full h-full absolute inset-0 z-0'>
-        <Canvas className='w-full h-full'>
+      <div className='w-full h-full flex justify-center items-center absolute inset-0 z-0'>
+        <Canvas className='w-full h-full'  style={{ transform: 'translate(400px,100px)' }}>
           <Suspense fallback={<CanvasLoader />}>
             <PerspectiveCamera makeDefault position={[0, 0, 30]} />
             <OrbitControls enableZoom enablePan enableRotate />
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} color={'#9cdba6'} />
-            <HackerRoom scale={0.2} position={[0, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
+            <HackerRoom scale={0.1} position={[0, 0, 0]} rotation={[0, -Math.PI / 2, 0]} />
           </Suspense>
         </Canvas>
       </div>
