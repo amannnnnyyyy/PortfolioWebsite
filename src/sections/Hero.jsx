640a07f6c274abf94acfb02ba3e513279862b908
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-26 15:14:33 +0300

    update

--- src/sections/Hero.jsx
+++ src/sections/Hero.jsx
@@ -6,6 +6,7 @@ import CanvasLoader from '../components/CanvasLoader';
 import { Leva, useControls } from 'leva';
 import { useMediaQuery } from 'react-responsive';
 import { calculateSizes } from '../constants';
+import Target from '../components/Target';
 
 const Hero = () => {
   const isMobile = useMediaQuery({query: '(max-width: 768px)'})
@@ -82,7 +83,7 @@ const Hero = () => {
                 scale = {sizes.deskScale}
             />
             <group>
-              
+              <Target position={sizes.targetPosition}/>
             </group>
             <ambientLight intensity={1} />
             <directionalLight position={[10, 10, 10]} intensity={0.5} color={'#9cdba6'} />
