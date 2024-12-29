4feccb19146281811fda45cb6ba323308984c732
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-29 08:54:19 +0300

    monitor content

--- src/components/HackerRoom.jsx
+++ src/components/HackerRoom.jsx
@@ -10,9 +10,7 @@ const HackerRoom=(props)=> {
   const { nodes, materials } = useGLTF('/models/hacker-room.glb');
 
   const monitortxt = useTexture('textures/desk/monitor.png');
-  const screenTxt = useTexture('textures/desk/screen.png');
-
-  const monitorTexture = useTexture('textures/desk/monitor.png');
+  const screenTxt = useTexture('textures/desk/me.jpg');
 
   return (
     <group {...props} dispose={null}>
