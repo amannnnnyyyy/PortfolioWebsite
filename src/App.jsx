a1775928b9414f7ac29a7d2285ba1678ed0b82ef
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-30 13:57:52 +0300

    about

--- src/App.jsx
+++ src/App.jsx
@@ -2,12 +2,14 @@ import React from 'react'
 import NavBar from './sections/NavBar'
 import Contents from './components/Contents'
 import Hero from './sections/Hero'
+import About from './sections/About'
 
 const App = () => {
   return (
     <main className='max-w-7xl mx-auto'>
       <NavBar/>
       <Hero/>
+      <About/>
     </main>
   )
 }
