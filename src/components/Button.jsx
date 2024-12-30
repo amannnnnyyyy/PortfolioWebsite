94571512b31bf7ced5e71b404cc7ea7515abfec6
Amanuel Moha <amanuelmoha.official@gmail.com>
2024-12-30 10:18:05 +0300

    Hero finishing

--- src/components/Button.jsx
+++ src/components/Button.jsx
@@ -4,8 +4,9 @@ const Button = ({name, isBeam=false, containerClass}) => {
   return (
     <button className={`btn $${containerClass}`}>
         {isBeam &&(
-            <span>
-                
+            <span className='relative flex h-3 w-3'>
+                <span className='btn-ping'/>
+                <span className='btn-ping_dot'/>
             </span>
         )}
         {name}
