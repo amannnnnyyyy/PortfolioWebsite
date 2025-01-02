7c7836e8d597e7d4c12f1fda4478ea9c147baba0
Amanuel Moha <mohaaman922@gmail.com>
2025-01-02 16:30:33 +0300

    display reviewer names

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -3,12 +3,12 @@ import { clientReviews } from '../constants'
 
 const Clients = () => {
   return (
-    <section className='c-space my-20'>
+    <section className='c-space my-20' id='contact'>
         <h3 className='head-text'>Hear from My Clients</h3>
 
         <div className='client-container'>
-            {clientReviews.map((item)=>(
-                <div key={item.id}></div>
+            {clientReviews.map(({id,name,review,img,position})=>(
+                <div key={id} className='client-review'>{name}</div>
             ))}
         </div>
     </section>
