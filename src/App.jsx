3f7350640829d56346fd260a4a6fb71039f1fec6
Amanuel Moha <mohaaman922@gmail.com>
2025-01-02 16:25:26 +0300

    clients added

--- src/App.jsx
+++ src/App.jsx
@@ -4,6 +4,7 @@ import Contents from './components/Contents'
 import Hero from './sections/Hero'
 import About from './sections/About'
 import Projects from './sections/Projects'
+import Clients from './sections/Clients'
 
 const App = () => {
   return (
@@ -12,6 +13,7 @@ const App = () => {
       <Hero/>
       <About/>
       <Projects/>
+      <Clients/>
     </main>
   )
 }
