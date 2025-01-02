bb46d5d7e171daeef01410789bdcd325b580f502
Amanuel Moha <mohaaman922@gmail.com>
2025-01-02 16:28:09 +0300

    import reviews

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -1,11 +1,16 @@
 import React from 'react'
+import { clientReviews } from '../constants'
 
 const Clients = () => {
   return (
     <section className='c-space my-20'>
         <h3 className='head-text'>Hear from My Clients</h3>
 
-        <div className='client-container'></div>
+        <div className='client-container'>
+            {clientReviews.map((item)=>(
+                <div key={item.id}></div>
+            ))}
+        </div>
     </section>
   )
 }
