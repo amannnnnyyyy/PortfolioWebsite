b32d6522c3a4055cc0e5fb422b3a7d9c46cce964
Amanuel Moha <mohaaman922@gmail.com>
2025-01-02 16:26:39 +0300

    testimonial text

--- src/sections/Clients.jsx
+++ src/sections/Clients.jsx
@@ -2,7 +2,9 @@ import React from 'react'
 
 const Clients = () => {
   return (
-    <section className='c-space my-20'>Clients</section>
+    <section className='c-space my-20'>
+        <h3 className='head-text'>Hear from My Clients</h3>
+    </section>
   )
 }
 
