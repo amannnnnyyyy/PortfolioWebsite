a149104be922d0979f81a7c228d901d17f531332
Amanuel Moha <mohaaman922@gmail.com>
2025-01-03 15:30:07 +0300

    header

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -2,7 +2,11 @@ import React from 'react'
 
 const Experience = () => {
   return (
-    <section>Experience</section>
+    <section className='c-space my-20'>
+        <div className='w-full text-white-600'>
+            <h3 className='head-text'>My Work Experience</h3>
+        </div>
+    </section>
   )
 }
 
