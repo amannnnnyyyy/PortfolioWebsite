de2a44350ec0e781ac91d07e3e5377f72d0d7bf2
Amanuel Moha <mohaaman922@gmail.com>
2025-01-03 15:21:41 +0300

    footer

--- src/sections/Footer.jsx
+++ src/sections/Footer.jsx
@@ -2,7 +2,18 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <section className='c-space pt-7 pb-3 border-black-300'>Footer</section>
+    <section className='c-space pt-7 pb-3 border-black-300 flex justify-between items-center flex-wrap gap-5'>
+        <div className='text-white-500 flex gap-2'>
+            <p>Terms & Conditions</p>
+            <p>|</p>
+            <p>Privacy policy</p>
+        </div>
+        <div className='flex gap-3'>
+            <div className='social-icon'>
+               <a href="https://github.com/amannnnnyyyy/"><img src="/assets/github.svg" alt="github" className='ml-3 w-1/2 h-1/2'/></a>
+            </div>
+        </div>
+    </section>
   )
 }
 
