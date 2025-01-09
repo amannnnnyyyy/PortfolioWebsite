6ecceb83562b7a94545286297e9cc36c2830b1d1
Amanuel Moha <mohaaman922@gmail.com>
2025-01-09 11:56:14 +0300

    developer model component

--- src/sections/Experience.jsx
+++ src/sections/Experience.jsx
@@ -16,7 +16,9 @@ const Experience = () => {
                         <spotLight position={[10,10,10]} angle={0.15} penumbra={1}/>
                         <directionalLight position={[10,10,10]} intensity={1}/>
                         <OrbitControls enableZoom = {false} maxPolarAngle={Math.PI/2}/>
-                        <Suspense fallback={<CanvasLoader/>}/>
+                        <Suspense fallback={<CanvasLoader/>}>
+                            
+                        </Suspense>
                     </Canvas>
                 </div>
                 <div className='work-content'>
